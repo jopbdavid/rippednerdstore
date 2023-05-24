c19b72f7319765bbd3ecc611b19d6213aa713695
jopbdavid <jopbdavid@gmail.com>
2023-05-24 10:32:25 +0100

    Homepage nav and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages";
+import { Home } from "./pages";
+import { Navbar, Footer } from "./components";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CartButtons from "./CartButtons";
+import Hero from "./Hero";
+import FeaturedProducts from "./FeaturedProducts";
+import Services from "./Services";
+import Contact from "./Contact";
+import Error from "./Error";
+import Loading from "./Loading";
+import Product from "./Product";
+
+export {
+  Navbar,
+  Footer,
+  CartButtons,
+  Hero,
+  FeaturedProducts,
+  Services,
+  Contact,
+  Error,
+  Loading,
+  Product,
+};
